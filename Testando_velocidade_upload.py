79f29e917e2084a36095a55c4f95666e95698611
Alexander Quincoces <alexanderquincoces@gmail.com>
2021-03-28 21:39:20 -0300

    Referencia

---
+++ Testando_velocidade_upload.py
@@ -0,0 +1,14 @@
+# Após dar run no code, existe um intervalo de tempo até sair o resultado, onde
+# O código vai estar usando o site speedtest para verificar as informações.
+from speedtest import Speedtest
+
+st = Speedtest()
+
+vel_download_bits = (st.download() / 1024) / 1024
+vel_upload = (st.upload() / 1024) / 1024
+
+print(f'O seu download: {vel_download_bits:.2f}MB/S')
+
+print(f'O seu upload: {vel_upload:.2f}MB/S')
+
+# Copiei o código para treinar desse video: https://www.youtube.com/watch?v=gMGizUbhX_o
